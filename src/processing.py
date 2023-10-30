fe585d6e136c56d86ca999ce2e0aa7388c8c0790
Evgeny Safonov <jeka-luda@yandex.ru>
2023-10-30 22:34:45 +0300

    add func get_list_dictionaries_sorted

--- src/processing.py
+++ src/processing.py
@@ -10,3 +10,13 @@ def get_list_dictionary_by_key(list_dictionaries: list, state: str = 'EXECUTED')
     return result_list
 
 
+def get_list_dictionaries_sorted(list_dictionaries: list) -> list:
+    """
+    Функция принимает на вход список словарей и возвращает новый список, в котором исходные словари отсортированы
+    по убыванию даты (ключ date). Функция принимает два аргумента, второй необязательный задает порядок сортировки
+    (убывание, возрастание)
+    :param list_dictionaries: список словарей
+    :return: список отсортированных словарей
+    """
+    sorted_dictionaries = sorted(list_dictionaries, key=lambda dictionary: dictionary['date'], reverse=True)
+    return sorted_dictionaries
