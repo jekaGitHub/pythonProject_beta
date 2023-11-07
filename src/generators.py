0fe43b4b2e439753e63444831ae57e39493bcfac
Evgeny Safonov <jeka-luda@yandex.ru>
2023-11-07 22:39:09 +0300

    add file generators.py and function transaction_descriptions

--- src/generators.py
+++ src/generators.py
@@ -1,12 +1,22 @@
 def filter_by_currency(transactions_list: list, currency: str):
     """
-    Принимает список словарей и возвращает итератор, выдающий по очереди операции с заданной валютой
+    Принимает список словарей и возвращает итератор, выдающий по очереди операции с заданной валютой.
     :param transactions_list: список словарей
     :param currency: валюта, по которой нужно отфильтровать словари
     :return: inerator, который выдаёт по очереди операции, в которых указана заданная валюта
     """
     
     return filter(lambda x: x["operationAmount"]["currency"]["code"] == currency, transactions_list)
+
+
+def transaction_descriptions(transaction_list: list):
+    """
+    Генератор, который принимает список словарей и возвращает описание каждой операции по очереди.
+    :param transaction_list: список словарей
+    :return: итератор описания каждой операции по очереди
+    """
+    for item in transaction_list:
+        yield item["description"]
 
 
 transactions = (
@@ -88,3 +98,4 @@ transactions = (
         }
     ]
 )
+
