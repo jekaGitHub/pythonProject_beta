f9c10e760700f09fcba525019612dfe67a5218dc
Evgeny Safonov <jeka-luda@yandex.ru>
2023-11-08 14:25:36 +0300

    add type in functions filter_by_currency and transaction_descriptions

--- src/generators.py
+++ src/generators.py
@@ -1,17 +1,18 @@
 import random
+from typing import Generator, Iterator
 
 
-def filter_by_currency(transactions_list: list, currency: str):
+def filter_by_currency(transactions_list: list, currency: str) -> Iterator:
     """
     Принимает список словарей и возвращает итератор, выдающий по очереди операции с заданной валютой.
     :param transactions_list: список словарей
     :param currency: валюта, по которой нужно отфильтровать словари
-    :return: inerator, который выдаёт по очереди операции, в которых указана заданная валюта
+    :return: generator, который выдаёт по очереди операции, в которых указана заданная валюта
     """
     return filter(lambda x: x["operationAmount"]["currency"]["code"] == currency, transactions_list)
 
 
-def transaction_descriptions(transaction_list: list):
+def transaction_descriptions(transaction_list: list) -> Generator:
     """
     Генератор, который принимает список словарей и возвращает описание каждой операции по очереди.
     :param transaction_list: список словарей
