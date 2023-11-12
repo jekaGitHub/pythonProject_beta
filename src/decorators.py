37df91c5e83731aeb1a979c7400017ba794eba54
Evgeny Safonov <jeka-luda@yandex.ru>
2023-11-12 22:26:04 +0300

    add decorators.py

---
+++ src/decorators.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from typing import Any, Callable, Optional
+
+
+def log(filename: Optional[str] = None) -> Callable:
+    def wrapper(func: Callable) -> Callable:
+        def inner(*args: Any, **kwargs: Any) -> Any:
+            now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            try:
+                result = func(*args, **kwargs)
+                log_message = f"{now} {func.__name__} ok\n"
+            except Exception as err:
+                log_message = f"{now} {func.__name__} error: {type(err).__name__}. Inputs: {args}, {kwargs}\n"
+                result = None
+            if filename:
+                with open(filename, "a") as file:
+                    file.write(log_message)
+            else:
+                print(log_message)
+            return result
+        return inner
+    return wrapper
+
+
+@log()
+def my_function(x, y):
+    return x + y
+
+
+print(my_function(2, 2))
